5fc871064de90afc737e90f3d45bd085a26d728e
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2021-10-27 20:33:08 -0700

    Fixed HTML ids

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,16 @@
 var currentDayEl = $("#currentDay")
 
 currentDayEl.text(moment().format("LL"));
+
+/*
+Array of textarea elements
+function that checks time
+if x hours after 8AM, turn that textarea red, turn less than x hours before gray
+all other boxes green
+
+button saves to local storage
+object with each hour as a property
+
+init() runs on load, fills in boxes with localstorage stuff
+
+*/

33c3a2a53bbe8828a67d36c78a4f7a35b1fe0646
Anguswaffle <90980870+Anguswaffle@users.noreply.github.com>
2021-10-30 16:36:42 -0700

    Added color change and message storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var containerEl = $(".container")
 var saveBtns = document.querySelectorAll(".saveBtn");
 var textElArray = [];
 var meetingsArray = [];
+var test = 0;
 
 
 currentDayEl.text(moment().format("LL"));
@@ -20,40 +21,82 @@ init() runs on load, fills in boxes with localstorage stuff
 
 */
 
+// Sets background colors depending on time of day
+function setColors() {
 
-// Determines how many hours have passed since 8:00 AM
-var startTime = moment().hour(8).minutes(0).seconds(0).milliseconds(0).format();
-console.log(startTime)
+    // Determines how many hours have passed since 9:00 AM
+    var startTime = moment().hour(09).minutes(0).seconds(0).milliseconds(0).format();
+    var timePassed = moment(startTime).startOf(moment()).fromNow().charAt(0);
+    var hoursLeft = 9;
 
-console.log(moment(startTime).startOf(moment()).fromNow())
+    console.log(timePassed);
+    // Adds past class to text areas in the past
+    for(var i = 0 ; i < timePassed ; i++){
+        textElArray[i].addClass("past");
+        hoursLeft--;
+    }
 
+    // Adds present class to text area in the active hour
+    if(timePassed < 9) {
+    textElArray[timePassed - 1].addClass("present");
+    }
+    // Adds future class to text areas coming up
+    while(hoursLeft > 0){
+        textElArray[9 - hoursLeft].addClass("future");
+        hoursLeft--;
+    }
+
+}
+
+
+// Populates text areas with saved
 function populateText() {
-
     for (var i = 0 ; i < textElArray.length ; i++){
         $(textElArray[i]).val(meetingsArray[i]);
     }
 }
 
-function saveSchedule(event) {
-    event.preventDefault();
-    
+function saveSchedule(target) {
+    var textArea = target.previousElementSibling;
+    var textID = textArea.parentElement.id;
+
+    meetingsArray[textID] = textArea.value;
+    localStorage.setItem('meetings', JSON.stringify(meetingsArray));
 }
 
+
+// Runs on page load
 function init() {
-    textElArray = [];
-    meetingsArray = [];
-    for(var i = 0 ; i < containerEl.children().length ; i ++){
-        textElArray.push(containerEl.children().eq(i).children().eq(1));
-        meetingsArray.push("");
-    }
-    
+
+    // All save buttons are given eventListener
+    var saveBtn = $(".saveBtn")
+    saveBtn.click(function(event){
+        event.preventDefault();
+        saveSchedule(this);
+    })
+
+    // Checks to see if localStorage exists
+    // If not, creates empty local storage and makes an empty meetingsArray
     if (JSON.parse(localStorage.getItem('meetings')) === null){
+        for(var i = 0 ; i < containerEl.children().length ; i ++){
+            textElArray.push(containerEl.children().eq(i).children().eq(1));
+            meetingsArray.push("");
+        }
         localStorage.setItem('meetings', JSON.stringify(meetingsArray));
+
+    // If local storage exists, values are retrieved and populate text areas
     } else {
+        for(var i = 0 ; i < containerEl.children().length ; i ++){
+            textElArray.push(containerEl.children().eq(i).children().eq(1));
+        }
+        meetingsArray = JSON.parse(localStorage.getItem('meetings'));
         populateText();
     }
+
+    setColors();
 }
 
-// saveBtns.addEventListener('click', saveSchedule);
+setInterval(setColors, 15000)
+
 
 init();
